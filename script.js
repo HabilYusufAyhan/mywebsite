f5f7137d763bd084c68bf1e18cd63001a909bd11
Habil Yusuf Ayhan <habilyusuf20@gmail.com>
2022-11-25 11:04:10 +0300

    animation cancelled

--- script.js
+++ script.js
@@ -6,20 +6,7 @@ menu.style.opacity = '0';
 menu.style.zIndex = '-1';
 
 var sayac = 0;
-var a =0;
-if (a < 5) {
-    setInterval(giris,1000);
-}
-function giris() {
-    analogo.style.width='500';
-    a++;
-    cikis();
-    
-   
-}
-function cikis() {
-    analogo.style.width='600';
-}
+
 
 burger.onclick=function(){
   
