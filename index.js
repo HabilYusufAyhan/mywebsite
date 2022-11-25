09618a2d3fe51b479ed2a433842b0d814f5a8ed6
Habil Yusuf Ayhan <habilyusuf20@gmail.com>
2022-11-25 15:07:24 +0300

    düzenleme

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 const burger = document.querySelector(".navbar-burger");
 const menu = document.querySelector(".menu");
 const gizle = document.querySelector(".gizle");
-const logo = document.querySelector(".navbar");
 const socialmedia = document.querySelector(".socialmedia");
-menu.style.opacity = "0";
-menu.style.zIndex = "-999";
+menu.style.opacity = '0';
+menu.style.zIndex = '-999';
 
 
 var sayac = 0;
 
 burger.onclick = function () {
   if (sayac === 0) {
-    menu.style.zIndex = "3";
-    menu.style.opacity = "1";
-    gizle.style.opacity = "0";
+    menu.style.zIndex = '3';
+    menu.style.background = 'rgba(0, 0, 0, 0.5)'
+    menu.style.opacity = '1';
+    gizle.style.opacity = '0';
+    socialmedia.style.opacity='0';
+    socialmedia.style.cursor='none'
     sayac++;
   } else if (sayac === 1) {
-    menu.style.zIndex = "-3";
-    menu.style.opacity = "0";
-    gizle.style.opacity = "1";
+    menu.style.zIndex = '-3';
+    menu.style.background = 'transparent'
+    menu.style.opacity = '0';
+    gizle.style.opacity = '1';
+    socialmedia.style.opacity='1'; 
+    socialmedia.style.cursor='pointer'
     sayac--;
   }
 };
+
