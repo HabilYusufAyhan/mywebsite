9418a56fadf7541ba5a04460efec40641510722a
Habil Yusuf Ayhan <habilyusuf20@gmail.com>
2022-11-27 00:37:30 +0300

    whoamı created

--- javascript/whoamı.js
+++ javascript/whoamı.js
@@ -24,3 +24,9 @@ burger.onclick = function () {
     sayac--;
   }
 };
+var typing=new Typed(".text", {
+  strings: ["", "Youtuber", "Freelancer", "Graphics Designer", "Web Designer", "Web Developer"],
+  typeSpeed: 100,
+  backSpeed: 40,
+  loop: true,
+});
