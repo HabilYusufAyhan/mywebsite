d9084e0a39ae172511b45fa77f1a72aa9ebd5769
Habil Yusuf Ayhan <habilyusuf20@gmail.com>
2023-02-14 13:29:26 +0300

    update

--- javascript/myprojects.js
+++ javascript/myprojects.js
@@ -1,6 +1,8 @@
 const burger = document.querySelector(".navbar-burger");
 const menu = document.querySelector(".menu");
 const socialmedia = document.querySelector(".socialmedia");
+const projectssection = document.querySelector(".projects-section");
+const title = document.querySelector('.title');
 menu.style.opacity = "0";
 menu.style.zIndex = "-999";
 const api = "https://api.github.com/users/HabilYusufAyhan/repos";
@@ -13,6 +15,8 @@ burger.onclick = function () {
     menu.style.opacity = "1";
     socialmedia.style.opacity = "0";
     socialmedia.style.cursor = "none";
+    projectssection.style.opacity = '0';
+    title.style.opacity = '0';
     sayac++;
   } else if (sayac === 1) {
     menu.style.zIndex = "-3";
@@ -20,11 +24,12 @@ burger.onclick = function () {
     menu.style.opacity = "0";
     socialmedia.style.opacity = "1";
     socialmedia.style.cursor = "pointer";
+    projectssection.style.opacity = '1';
+    title.style.opacity = '1'
     sayac--;
   }
 };
 
-const projectssection = document.querySelector(".projects-section");
 user(api);
 async function user(api) {
   const resp = await fetch(api);
@@ -52,21 +57,36 @@ async function user(api) {
     for (let datalang in langdata) {
    
      
-      //sayi = langdata[datalang]+sayi;
-    //  var yuzdelik = (100 * langdata[datalang])/sayi
-    //  console.log(langdata[datalang]);
-    //  console.log(sayi);
-    //  console.log(yuzdelik);
+
       const adduser3 = document.createElement("p");
       adduser3.classList.add('language');
-      adduser3.textContent = `${datalang} = ${langdata[datalang]}`
+      adduser3.innerHTML = `<span>${datalang}</span> = <span>${langdata[datalang]}</span>`
       
       projectssection.appendChild(adduser);
       adduser.appendChild(adduser2);
       adduser2.appendChild(adduser3);
     }
-    if (respData[index].description == 'under development') {
+    if (respData[index].description.toLocaleLowerCase() == 'under development') {
       projectssection.childNodes[index+1].firstElementChild.style.color = 'yellow'
+    }
+
+
+
+  
+  }
+  for (let yuzde = 0; yuzde < respData.length; yuzde++) {
+    var toplam = 0;
+    for (let childlang = 0; childlang < projectssection.children[yuzde].lastElementChild.children.length; childlang++) {
+     toplam = Math.floor(projectssection.children[yuzde].lastElementChild.children[childlang].lastElementChild.textContent) + toplam;
+    
+    }
+    console.log(toplam);
+    for (let designlang = 0; designlang < projectssection.children[yuzde].lastElementChild.children.length; designlang++) {
+      var oranlar =  Math.floor(projectssection.children[yuzde].lastElementChild.children[designlang].lastElementChild.textContent);
+      var yuzdelik = (100*oranlar)/toplam;
+      projectssection.children[yuzde].lastElementChild.children[designlang].lastElementChild.textContent = yuzdelik.toFixed(2)+  '%';
+      console.log(yuzdelik);
+      projectssection.children[yuzde].lastElementChild.children[designlang].style.opacity = '1';
     }
   }
 }
--- javascript/whoamı.js
+++ javascript/whoamı.js
@@ -1,6 +1,7 @@
 const burger = document.querySelector(".navbar-burger");
 const menu = document.querySelector(".menu");
 const socialmedia = document.querySelector(".socialmedia");
+const main = document.querySelector('.mainanam');
 menu.style.opacity = '0';
 menu.style.zIndex = '-999';
 
@@ -13,14 +14,16 @@ burger.onclick = function () {
     menu.style.background = 'rgba(0, 0, 0, 0.5)'
     menu.style.opacity = '1';
     socialmedia.style.opacity='0';
-    socialmedia.style.cursor='none'
+    socialmedia.style.cursor='none';
+    main.style.opacity = '0';
     sayac++;
   } else if (sayac === 1) {
     menu.style.zIndex = '-3';
     menu.style.background = 'transparent'
     menu.style.opacity = '0';
     socialmedia.style.opacity='1'; 
-    socialmedia.style.cursor='pointer'
+    socialmedia.style.cursor='pointer';
+    main.style.opacity = '1';
     sayac--;
   }
 };
