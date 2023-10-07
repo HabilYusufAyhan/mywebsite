853f11d861c7409ef40d694ad09dacc4ab186bdd
Habil Yusuf Ayhan <habilyusuf20@gmail.com>
2023-10-07 14:58:17 +0300

    github api added

--- src/routers/auth_router.js
+++ src/routers/auth_router.js
@@ -1,25 +1,11 @@
-const router = require('express').Router();
-const authController = require('../controllers/auth_controller');
-const validatorMiddleware = require('../middlewares/validation_middleware');
-const authMiddleware = require('../middlewares/auth_middleware');
+const router = require("express").Router();
+const authController = require("../controllers/auth_controller");
 
+router.get("/whoami", authController.whoami);
+router.get(
+  "/myprojects",
 
-router.get('/login', authMiddleware.oturumAcilmamis, authController.loginFormunuGoster);
-router.post('/login', authMiddleware.oturumAcilmamis, validatorMiddleware.validateLogin(), authController.login);
-
-router.get('/register', authMiddleware.oturumAcilmamis, authController.registerFormunuGoster);
-router.post('/register', authMiddleware.oturumAcilmamis, validatorMiddleware.validateNewUser(), authController.register);
-
-router.get('/forget-password',authMiddleware.oturumAcilmamis, authController.forgetPasswordFormunuGoster);
-router.post('/forget-password', authMiddleware.oturumAcilmamis, validatorMiddleware.validateEmail(), authController.forgetPassword);
-
-router.get('/verify', authController.verifyMail);
-
-
-router.get('/reset-password/:id/:token', authController.yeniSifreFormuGoster);
-router.get('/reset-password', authController.yeniSifreFormuGoster);
-router.post('/reset-password', validatorMiddleware.validateNewPassword(), authController.yeniSifreyiKaydet);
-router.get('/logout', authMiddleware.oturumAcilmis, authController.logout);
-
+  authController.myprojects
+);
 
 module.exports = router;
